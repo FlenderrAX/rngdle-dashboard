44b026e567f5d6063a9451fcb8088fd3092ca833
FlenderrAX <74816698+FlenderrAX@users.noreply.github.com>
2026-06-16 20:36:54 +0100

    Optimized registration + Added embed dedicated to registration

--- cogs/register.py
+++ cogs/register.py
@@ -14,24 +14,39 @@ class Register(commands.Cog):
     async def register(
         self,
         ctx: discord.ApplicationContext,
-        username: str
+        rngdle_username: str
     ):  
-        pass
-        # script_path = pathlib.Path(__file__).resolve()
-        # parent_path = script_path.parent.parent
-        # local_save = f"{parent_path}{os.sep}players.json"
+        guild_id = str(ctx.guild.id)
+        user_id = str(ctx.author.id)
 
-        # with open(local_save, "r") as file:
-        #     usernames = json.load(file)
+        script_path = pathlib.Path(__file__).resolve()
+        parent_path = script_path.parent.parent
+        local_save = f"{parent_path}{os.sep}players.json"
 
-        #     if str(ctx.user.id) in list(usernames.keys()): await ctx.respond("Already registrated"); return
+        try:
+            with open(local_save, "r") as file:
+                data = json.load(file)
+        except: data = {}
 
-        #     usernames[ctx.user.id] = username.lower()
+        if guild_id not in data:
+            data[guild_id] = {}
 
-        # with open(local_save, "w") as file:
-        #     json.dump(usernames, file)
+        data[guild_id][user_id] = rngdle_username
 
-        # await ctx.respond(f"Added {username}")
+        with open(local_save, "w") as f:
+            json.dump(data, f, indent=4)
+
+        embed = discord.Embed(
+            title="Succesfully registrated !",
+            description=f"You just linked your account to : `{rngdle_username}`.",
+            color=discord.Color.green()
+        )
+        
+        embed.set_thumbnail(url=ctx.author.display_avatar.url)
+        embed.add_field(name="Server", value=ctx.guild.name, inline=True)
+        embed.set_footer(text="RNGdle Bot • Use /leaderboard to see the leaderboard")
+
+        await ctx.respond(embed=embed)
 
 def setup(bot):
     bot.add_cog(Register(bot))
