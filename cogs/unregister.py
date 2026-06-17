3619a5deb00727549e201d94e3109f01953a4dae
FlenderrAX <74816698+FlenderrAX@users.noreply.github.com>
2026-06-17 11:07:05 +0100

    Restructured unregistration process : Removed verification by argument but directly from the command author

--- cogs/unregister.py
+++ cogs/unregister.py
@@ -1,6 +1,7 @@
 import discord
 from discord.commands import slash_command
 from discord.ext import commands
+
 import json
 import pathlib
 import os
@@ -13,28 +14,38 @@ class Unregister(commands.Cog):
     @slash_command(name='unregister')
     async def unregister(
         self,
-        ctx: discord.ApplicationContext,
-        username: str
+        ctx: discord.ApplicationContext
     ):  
+        guild_id = str(ctx.guild.id)
+        user_id = str(ctx.author.id)
+
         script_path = pathlib.Path(__file__).resolve()
         parent_path = script_path.parent.parent
-        local_save = f"{parent_path}{os.sep}players.json"
+        local_save = os.path.join(parent_path, "players.json")
+
+        try:
+            with open(local_save, "r") as f:
+                data = json.load(f)
+        except: data = {}
         
-        with open(local_save, "r") as f:
-            data = json.load(f)
-        
-        if str(ctx.user.id) not in list(data.keys()): 
-            await ctx.respond("You haven't registered your username yet!")
+        if guild_id not in data or user_id not in data[guild_id]:
+            await ctx.respond("You haven't registrated your account yet!", ephemeral=True)
             return
 
-        if username.lower() != data[str(ctx.user.id)]: 
-            await ctx.respond(f"You cannot unregister another user!\nYour username `{data[str(ctx.user.id)]}`")
-            return
-        
-        data.pop(str(ctx.user.id))
+        removed_username = data[guild_id].pop(user_id)
 
         with open(local_save, "w") as f:
-            json.dump(data, f)
+            json.dump(data, f, indent=4)
+
+        embed = discord.Embed(
+            title="Succesfully unregistrated !",
+            description=f"You just dissociated your account : `{removed_username}`.",
+            color=discord.Color.red()
+        )
+        
+        embed.set_thumbnail(url=ctx.author.display_avatar.url)
+        embed.add_field(name="Server", value=ctx.guild.name, inline=True)
+        embed.set_footer(text="RNGdle Bot • Use /leaderboard to see the leaderboard")
 
         await ctx.respond(data)
     
